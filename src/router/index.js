f2e73b0981e7ec97cfed72a671cbe6dfe9fd2cc9
lqd_git <13667737036@163.com>
2019-12-09 19:30:27 +0800

    路由嵌套实现

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,34 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes: {
     path: '/',
-    component: () => import('@/views/Main.vue')
+    component: () => import('@/views/Main.vue'),
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/Home/Home.vue')
+      },
+      {
+        path: 'user',
+        name: 'user',
+        component: () => import('@/views/UserManage/UserManage.vue')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/VideoManage/VideoManage.vue')
+      },
+      {
+        path: 'one',
+        name: 'one',
+        component: () => import('@/views/Other/PageOne.vue')
+      },
+      {
+        path: 'two',
+        name: 'two',
+        component: () => import('@/views/Other/PageTwo.vue')
+      }
+    ]
   }
 })
 
