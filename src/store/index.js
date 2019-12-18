20954226eec97b3a51b5548e1758532a6323b9ec
lqd123456 <43326509+lqd123456@users.noreply.github.com>
2019-12-18 21:57:01 +0800

    Update index.js
    asdsad

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import tab from './tab'
-import user from './user'
+import user22 from './user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     tab,
-    user
+    user22
   }
 })
 
